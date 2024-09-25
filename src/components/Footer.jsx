b86b642a2f6bd993c2aa031807c811fe90ccfa74
opeyemi <opeyemiodunayo2018@gmail.com>
2024-09-25 00:21:48 -0700

    i change bg-color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       className={`${
         themeMode === "light"
           ? "bg-[#f3f4f6] text-[#111827]"
-          : "bg-[#030712] text-[#f3f4f6]"
+          : "bg-[#111827] text-[#f3f4f6]"
       }`}
     >
       <h1 className="text-center sm:p-4 p-2 sm:font-2xl text-[16px]">
